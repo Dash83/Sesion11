a5bc66d41680e92576f3bf793d58ba98d88c7ef1
Marco Caballero <caballeromarco@gmail.com>
2015-04-14 22:12:22 -0500

    Initial commit

---
+++ app/src/main/java/com/iteso/marco/sesion11/MyService.java
@@ -0,0 +1,41 @@
+package com.iteso.marco.sesion11;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by marco on 4/14/15.
+ */
+public class MyService extends Service
+{
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        Toast.makeText(this, "Service Created", Toast.LENGTH_LONG).show();
+        Log.v("Services", "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        Log.v("Services", "onStart");
+        return Service.START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+        Log.v("Services", "onDestroy");
+    }
+}
